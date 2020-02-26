83fce429f590a2191380b587e0d3b763ef45ed8a
moira <johnandmoira@live.co.u>
2020-02-26 10:49:08 +0000

    completed

--- specs/drink_spec.rb
+++ specs/drink_spec.rb
@@ -3,6 +3,13 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative("../Drink")
 class DrinkTest < MiniTest::Test
+  def setup()
+    @drink1 = Drink.new("Wine-white", 500)
+    @drink2 = Drink.new("Wine-red", 750)
+  end
 
+    def test_drink_has_name
+      assert_equal("Wine-white", @drink1.name)
+    end
 
-end 
+end
